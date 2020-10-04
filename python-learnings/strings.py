b07471bd119e46b80dd4f2b8b7d16f1d199af5ba
Akshath S <akshath@nobso.com>
2020-10-03 23:35:25 -0700

    exploring string methods

---
+++ python-learnings/strings.py
@@ -0,0 +1,37 @@
+# Python String capitalize() Method
+# Converts the first character to upper case
+txt = "hello, and welcome to my world."
+x = txt.capitalize()
+print (x)
+
+# Python String casefold() Method
+# Converts string into lower case
+gaming = "I like doing stuff"
+x = gaming.casefold()
+print(x)
+
+# Python String center() Method
+# Returns a centered string
+txt = "banana"
+
+x = txt.center(20)
+
+print(x)
+
+# Python String count() Method
+# Returns the number of times a specified value occurs in a string
+fruit = "I love apples, apple are my favorite fruit"
+
+x = fruit.count("apple")
+
+print(x)
+
+
+# python String find() Method
+# Searches the string for a specified value and returns the position of where it was found
+txt = "Hello, welcome to my world."
+
+x = txt.find("welcome")
+
+print(x)
+
